c84acdc6409e4b4b429700846e0d8dc4c61a4e27
thcheetah777 <thcheetah777@gmail.com>
2023-03-29 16:33:42 +0900

    Wow

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,20 +8,34 @@ public class PlayerMovement : MonoBehaviour
 
     // Stats
     [SerializeField] private float thrustForce = 10;
+    [SerializeField] private float boostingThrustForce = 20;
+    [SerializeField] private float boostingDrag = 5;
     [SerializeField] private float turnSpeed = 5;
 
     // Input
     private float thrustInput;
     private float turnInput;
+    private float boostInput;
 
     Rigidbody2D playerBody;
+    float originalDrag;
 
     void Start() {
         playerBody = GetComponent<Rigidbody2D>();
+
+        originalDrag = playerBody.drag;
     }
 
     void FixedUpdate() {
-        playerBody.AddRelativeForce(Vector2.up * thrustInput * thrustForce);
+        if (boostInput > 0)
+        {
+            playerBody.AddRelativeForce(Vector2.up * thrustInput * boostingThrustForce);
+            playerBody.drag = boostingDrag;
+        } else
+        {
+            playerBody.AddRelativeForce(Vector2.up * thrustInput * thrustForce);
+            playerBody.drag = originalDrag;
+        }
         playerBody.AddTorque(turnInput * turnSpeed);
     }
 
@@ -31,6 +45,10 @@ public class PlayerMovement : MonoBehaviour
 
     void OnThrust(InputValue value) {
         thrustInput = value.Get<float>();
+    }
+
+    void OnBoost(InputValue value) {
+        boostInput = value.Get<float>();
     }
 
 }
