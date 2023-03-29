481619a16b77f47e948f16ade22080ef525497cf
thcheetah777 <thcheetah777@gmail.com>
2023-03-29 17:42:34 +0900

    Add boosting

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,9 +7,19 @@ public class PlayerMovement : MonoBehaviour
 {
 
     // Stats
+    [Header("Thrusting")]
     [SerializeField] private float thrustForce = 10;
     [SerializeField] private float thrustingDrag = 20;
+
+    [Header("Boosting")]
+    [SerializeField] private float boostingForce = 10;
+    [SerializeField] private float boostStartForce = 5;
+    [SerializeField] private ParticleSystem boostingParticles;
+
+    [Header("Turning")]
     [SerializeField] private float turnSpeed = 5;
+    [SerializeField] private float thrustingTurnSpeed = 1.5f;
+    [SerializeField] private float thrustingAngularDrag = 20;
 
     // Input
     private float thrustInput;
@@ -18,18 +28,24 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D playerBody;
     float originalDrag;
+    private float originalAngularDrag;
 
     void Start() {
         playerBody = GetComponent<Rigidbody2D>();
 
         originalDrag = playerBody.drag;
+        originalAngularDrag = playerBody.angularDrag;
     }
 
     void FixedUpdate() {
         playerBody.drag = thrustingDrag * thrustInput;
         playerBody.gravityScale = (int)thrustInput ^ 1;
-        playerBody.AddRelativeForce(Vector2.up * thrustInput * thrustForce);
-        playerBody.AddTorque(turnInput * turnSpeed);
+        playerBody.angularDrag = thrustInput > 0 ? thrustingAngularDrag : originalAngularDrag;
+
+        float force = thrustInput * (boostInput > 0 ? boostingForce : thrustForce);
+
+        playerBody.AddRelativeForce(Vector2.up * force);
+        playerBody.AddTorque(turnInput * (thrustInput > 0 ? thrustingTurnSpeed : turnSpeed));
     }
 
     void OnRotation(InputValue value) {
@@ -42,6 +58,13 @@ public class PlayerMovement : MonoBehaviour
 
     void OnBoost(InputValue value) {
         boostInput = value.Get<float>();
+        if (boostInput > 0 && thrustInput > 0)
+        {
+            playerBody.AddRelativeForce(Vector2.up * boostStartForce, ForceMode2D.Impulse);
+            boostingParticles.Play();
+        } else if (boostInput == 0) {
+            boostingParticles.Stop();
+        }
     }
 
 }
