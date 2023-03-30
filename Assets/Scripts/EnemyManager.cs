9e67bed5dcfd22fd92d8a6bcc083620edd455faf
thcheetah777 <thcheetah777@gmail.com>
2023-03-30 17:29:58 +0900

    Actually spawn enemies

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,18 +7,32 @@ public class EnemyManager : MonoBehaviour
 
     [SerializeField] private int enemyCount = 5;
     [SerializeField] private FollowEnemy enemyPrefab;
+    [SerializeField] private float enemySpawnDelay = 1;
 
     BoxCollider2D box;
 
     void Start() {
         box = GetComponent<BoxCollider2D>();
+        StartCoroutine(EnemySpawnRoutine());
 
         for (int i = 0; i < enemyCount; i++)
         {
-            float x = Random.Range(box.bounds.min.x, box.bounds.max.x);
-            float y = Random.Range(box.bounds.min.y, box.bounds.max.y);
+            SpawnEnemy();
+        }
+    }
 
-            FollowEnemy enemy = Instantiate(enemyPrefab, new Vector2(x, y), Quaternion.identity);
+    private void SpawnEnemy() {
+        float x = Random.Range(box.bounds.min.x, box.bounds.max.x);
+        float y = Random.Range(box.bounds.min.y, box.bounds.max.y);
+
+        FollowEnemy enemy = Instantiate(enemyPrefab, new Vector2(x, y), Quaternion.identity);
+    }
+
+    private IEnumerator EnemySpawnRoutine() {
+        while (true)
+        {
+            yield return new WaitForSeconds(enemySpawnDelay);
+            SpawnEnemy();
         }
     }
 
