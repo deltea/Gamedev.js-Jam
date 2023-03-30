dc168b71f1e86930f331f223f3ca4d95f2f4d4b4
thcheetah777 <thcheetah777@gmail.com>
2023-03-30 17:39:01 +0900

    Add health to player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField] private float maxHealth = 100;
+    
+    private float health;
+
+    void Start() {
+        health = maxHealth;
+    }
+
+    private void GetHurt(float damage) {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die() {
+        print("You DIED");
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger) {
+        if (trigger.CompareTag("Enemy"))
+        {
+            trigger.GetComponent<Enemy>().Die();
+            GetHurt(10);
+        }
+    }
+
+}
