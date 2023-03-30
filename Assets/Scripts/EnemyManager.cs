f3cd24f425499f0649c6c63a5865ee7c3a440956
thcheetah777 <thcheetah777@gmail.com>
2023-03-30 16:53:40 +0900

    Actual flocking

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,8 +7,6 @@ public class EnemyManager : MonoBehaviour
 
     [SerializeField] private int enemyCount = 5;
     [SerializeField] private FollowEnemy enemyPrefab;
-    [SerializeField] private float rotationSmoothingRange = 0.1f;
-    [SerializeField] private float speedRange = 1;
 
     BoxCollider2D box;
 
@@ -21,8 +19,6 @@ public class EnemyManager : MonoBehaviour
             float y = Random.Range(box.bounds.min.y, box.bounds.max.y);
 
             FollowEnemy enemy = Instantiate(enemyPrefab, new Vector2(x, y), Quaternion.identity);
-            enemy.rotationSmoothing += Random.Range(-rotationSmoothingRange, rotationSmoothingRange);
-            enemy.speed += Random.Range(-speedRange, speedRange);
         }
     }
 
